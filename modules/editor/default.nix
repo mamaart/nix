7e4d027cf5a720310b607351352458df82d9b428
Martin Maartensson <martinmaartensson@gmail.com>
2024-09-12 12:08:50 +0200

    add symbols overview

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -8,6 +8,7 @@
     extraPlugins = with pkgs.vimPlugins; [ 
       vim-go 
       lazygit-nvim
+      aerial-nvim
     ];
 
 
@@ -38,6 +39,16 @@
 
 
     opts.completeopt = ["menu" "menuone" "noselect"];
+
+    extraConfigLua = ''
+    require("aerial").setup({
+      on_attach = function(bufnr)
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    })
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    '';
 
     extraConfigVim = ''
     augroup GolangMappings
